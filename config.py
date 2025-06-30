75a7a0623b112a14c016759d58618800329a67d2
«alexander» <alexander.mishin.2001@gmail.com>
2025-06-30 14:51:01 +0300

    C:/Program Files/Git/nutrition

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 import os
-
-# Telegram Bot
 BOT_TOKEN = "7144225641:AAEiEO5S0ofPreoftsuFJKiNToSWhEqNa-I"
-
-# GigaChat API
+GIGACHAT_TOKEN = ""
 GIGACHAT_AUTH_KEY = "NmU5ZjRjYTgtMTQ1OC00NTE5LTk0N2EtODBiZjFiZjE3ZTcyOjVmMzNiMGQ3LWZlMzQtNDc5MC05NTgxLTQ0OTIwOWJiODRiOA=="
-
-# Database
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///nutri.db")  # Fallback to SQLite
+KANDINSKY_TOKEN = "ваш_ключ_kandinsky"
+DATABASE_URL = os.getenv("DATABASE_URL")
